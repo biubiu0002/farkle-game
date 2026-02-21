1369077ae50cfe45a73c9098c6668b8977e99959
yeruijian <yeruijian@yeruijiandeMacBook-Pro.local>
2026-02-21 21:16:02 +0800

    feat: add 3D dice HTML generation helper functions

--- simple-game/utils/ui.js
+++ simple-game/utils/ui.js
@@ -3,6 +3,84 @@
  * 所有 UI 更新逻辑
  */
 
+/**
+ * 根据骰子数值生成点数布局数组
+ * @param {number} value - 骰子数值 (1-6)
+ * @returns {Array} - 9个元素的数组，1表示有点，0表示无点
+ */
+function getPipLayout(value) {
+  // 3x3网格索引:
+  // 0 1 2
+  // 3 4 5
+  // 6 7 8
+  const layouts = {
+    1: [0,0,0, 0,1,0, 0,0,0],           // 中心1个
+    2: [1,0,0, 0,0,0, 0,0,1],           // 对角2个
+    3: [1,0,0, 0,1,0, 0,0,1],           // 对角+中心
+    4: [1,0,1, 0,0,0, 1,0,1],           // 四角
+    5: [1,0,1, 0,1,0, 1,0,1],           // 四角+中心
+    6: [1,0,1, 1,0,1, 1,0,1]            // 两列
+  }
+  return layouts[value] || layouts[1]
+}
+
+/**
+ * 创建3D骰子的HTML结构
+ * @param {number} value - 骰子数值 (1-6)
+ * @param {number} index - 骰子索引
+ * @returns {HTMLElement} - 3D骰子DOM元素
+ */
+function create3DDie(value, index) {
+  const container = document.createElement('div')
+  container.className = 'die-3d-container'
+
+  const die = document.createElement('div')
+  die.className = 'die-3d'
+  die.dataset.value = value
+  die.dataset.index = index
+
+  // 根据 value 旋转骰子显示对应面
+  const rotations = {
+    1: 'rotateX(0deg) rotateY(0deg)',      // front
+    2: 'rotateX(0deg) rotateY(-90deg)',    // left
+    3: 'rotateX(0deg) rotateY(90deg)',     // right
+    4: 'rotateX(-90deg) rotateY(0deg)',    // top
+    5: 'rotateX(90deg) rotateY(0deg)',     // bottom
+    6: 'rotateX(180deg) rotateY(0deg)'     // back
+  }
+  die.style.transform = rotations[value] || rotations[1]
+
+  // 创建6个面，每个面显示1点
+  const faces = ['front', 'back', 'right', 'left', 'top', 'bottom']
+  const faceValues = [1, 6, 3, 2, 4, 5] // 每个面对应的数值
+
+  faces.forEach((faceName, i) => {
+    const face = document.createElement('div')
+    face.className = `die-face ${faceName}`
+
+    const pipsContainer = document.createElement('div')
+    pipsContainer.className = 'die-pips'
+
+    const layout = getPipLayout(faceValues[i])
+    layout.forEach(hasPip => {
+      const pip = document.createElement('div')
+      pip.className = 'pip'
+      if (hasPip) {
+        pip.style.visibility = 'visible'
+      } else {
+        pip.style.visibility = 'hidden'
+      }
+      pipsContainer.appendChild(pip)
+    })
+
+    face.appendChild(pipsContainer)
+    die.appendChild(face)
+  })
+
+  container.appendChild(die)
+  return container
+}
+
 function updateUI(gameState, selectedDiceIndices) {
   // 更新玩家得分表格
   document.getElementById('player0LastScore').textContent = gameState.players[0].lastRoundScore || 0
